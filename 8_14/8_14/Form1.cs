0ebfe43a60733d36ca3716806df4d42cbbd7a3ca
Mathew Paschall <XDarkZero89@hotmail.com>
2017-05-15 03:04:24 -0400

    Add files via upload

---
+++ 8_14/8_14/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_14
+{
+    public partial class ChessBoard : Form
+    {
+        public ChessBoard()
+        {
+            InitializeComponent();
+            Size = new Size(400, 400);
+            Text = "Chess";
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            int x = 10;
+            int y = 10;
+            int z = 10;
+            int h = 10;
+
+            Graphics g = e.Graphics;
+            Pen black = new Pen(Color.Black, 1);
+            for (int i = 0; i < 64; i++)
+            {
+                g.DrawRectangle(black, 10, 10, 80, 80);
+                while (y <= 80)
+                {
+                    g.DrawRectangle(black, x, y, z, h);
+                g.FillRectangle(Brushes.Black, x, y, z, h);
+                base.OnPaint(e);
+                x = x + 20;   
+                    if (x == 90 || x == 100)
+                    {
+                        y = y + 10;
+                        if (y == 20 || y == 40 || y == 60 || y == 80)
+                        {
+                            x = 20;
+                        }
+                        else
+                        {
+                            x = 10;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void main()
+        {
+            Application.Run(new ChessBoard());
+        }
+    }
+}
---
+++ 8_18/8_18/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_18
+{
+    public partial class Form1 : Form
+    {
+        private Font arial = new Font("Arial", 16, FontStyle.Bold);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int arialStart = (10);
+            g.DrawString("Tomato Soup Co.", arial, Brushes.Red, arialStart, 120);
+            Pen clear = new Pen(Color.Transparent, 1);
+            Pen black = new Pen(Color.Black, 1);
+            g.DrawRectangle(black, 40, 60, 25, 5);
+            g.FillRectangle(Brushes.Green, 40, 60, 25, 5);
+            g.DrawRectangle(clear, 50, 50, 50, 50);
+            g.DrawEllipse(black, 50, 50, 50, 50);
+            g.FillEllipse(Brushes.Red, 50, 50, 50, 50);
+            base.OnPaint(e);
+        }
+    }
+}
